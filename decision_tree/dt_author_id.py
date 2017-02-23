8d39e5843e40259327d3f6b5d744c5dedcac46eb
Andreas Lundgren <alundgren82@gmail.com>
2017-02-23 21:03:33 +0100

    Decision Trees exercise

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -19,13 +19,23 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
 #########################################################
 ### your code goes here ###
+from sklearn import tree
 
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
 
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s" #~49s
+
+t1 = time()
+pred = clf.predict(features_test)
+print "prediction time:", round(time()-t1, 3), "s" #~0.02s
+
+print "accuracy:", clf.score(features_test, labels_test) #.979
+
+print "number of features", len(features_train[0])
 #########################################################
 
 
